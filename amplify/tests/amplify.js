a5b788e84d0d14c74ff9eb01a2a120dd24af0c73
Jack Liell-Cock <jackliellcock@gmail.com>
2021-09-25 18:16:10 +1000

    update gitignore

--- amplify/tests/amplify.js
+++ amplify/tests/amplify.js
@@ -2,42 +2,41 @@ const assert = require("assert");
 const anchor = require("@project-serum/anchor");
 const { SystemProgram } = anchor.web3;
 
-describe("amplify", () => {
-  /* create and set a Provider */
+describe("Amplify", () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.Amplify;
-  it("Creates a counter)", async () => {
-    /* Call the create function via RPC */
+  it("It initializes the account", async () => {
     const baseAccount = anchor.web3.Keypair.generate();
-    await program.rpc.create({
+    await program.rpc.initialize("Hello World", {
       accounts: {
-        baseAccount: baseAccount.publicKey,
-        user: provider.wallet.publicKey,
+        request: baseAccount.publicKey,
+        requester: provider.wallet.publicKey,
         systemProgram: SystemProgram.programId,
       },
       signers: [baseAccount],
     });
 
-    /* Fetch the account and check the value of count */
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('Count 0: ', account.count.toString())
-    assert.ok(account.count.toString() == 0);
+    console.log('Data: ', account.data);
+    assert.ok(account.data === "Hello World");
     _baseAccount = baseAccount;
-
   });
 
-  it("Increments the counter", async () => {
+  it("Updates a previously created account", async () => {
     const baseAccount = _baseAccount;
 
-    await program.rpc.increment({
+    await program.rpc.update("Some new data", {
       accounts: {
         baseAccount: baseAccount.publicKey,
       },
     });
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('Count 1: ', account.count.toString())
-    assert.ok(account.count.toString() == 1);
+    console.log('Updated data: ', account.data)
+    assert.ok(account.data === "Some new data");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
   });
 });
